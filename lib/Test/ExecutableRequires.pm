5d1b0792331b466012aa5cbfa9bc7f9039f004b4
Dann <techmemo@gmail.com>
2010-03-15 21:53:01 +0900

    initial commit

---
+++ lib/Test/ExecutableRequires.pm
@@ -0,0 +1,47 @@
+package Test::ExecutableRequires;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::ExecutableRequires -
+
+=head1 SYNOPSIS
+
+  use Test::ExecutableRequires;
+
+=head1 DESCRIPTION
+
+Test::ExecutableRequires is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+  http://github.com/dann/
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+dann E<lt>techmemo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
